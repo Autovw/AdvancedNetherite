d50cef3b44fc62afa8f92454b6c4280f5e7554dd
Autovw <68228006+Autovw@users.noreply.github.com>
2021-06-30 14:16:17 +0200

    Added Config to Advanced Netherite

---
+++ src/main/java/com/autovw/advancednetherite/config/Config.java
@@ -0,0 +1,50 @@
+package com.autovw.advancednetherite.config;
+
+import net.minecraftforge.common.ForgeConfigSpec;
+import org.apache.commons.lang3.tuple.Pair;
+
+//@Mod.EventBusSubscriber
+public class Config {
+    public static final ForgeConfigSpec commonConfig;
+    public static final Config.Common COMMON;
+
+    static {
+        final Pair<Common, ForgeConfigSpec> commonConfigPair = new ForgeConfigSpec.Builder().configure(Common::new);
+        commonConfig = commonConfigPair.getRight();
+        COMMON = commonConfigPair.getLeft();
+    }
+
+    public static class Common {
+        public final ArmorConfig armorConfig;
+
+        public Common(ForgeConfigSpec.Builder builder) {
+            builder.push("common");
+            {
+                this.armorConfig = new ArmorConfig(builder);
+            }
+            builder.pop();
+        }
+    }
+
+    public static class ArmorConfig {
+        public static ForgeConfigSpec.BooleanValue goldPiglinPassiveArmor;
+        public static ForgeConfigSpec.BooleanValue emeraldEndermanPassiveArmor;
+        public static ForgeConfigSpec.BooleanValue diamondUltimatePassiveArmor;
+
+        public ArmorConfig(ForgeConfigSpec.Builder builder) {
+            builder.comment("Armor config").push("armor_config");
+            {
+                goldPiglinPassiveArmor = builder
+                        .comment("If true, Piglins behave neutral towards players wearing Netherite/Gold Armor. True by default.")
+                        .define("goldPiglinPassiveArmor", true);
+                emeraldEndermanPassiveArmor = builder
+                        .comment("If true, Endermans behave neutral towards players wearing Netherite/Emerald Armor. True by default.")
+                        .define("emeraldEndermanPassiveArmor", true);
+                diamondUltimatePassiveArmor = builder
+                        .comment("If true, Endermans and Piglins behave neutral towards players wearing Netherite/Diamond Armor. False by default.")
+                        .define("diamondUltimatePassiveArmor", false);
+            }
+            builder.pop();
+        }
+    }
+}
