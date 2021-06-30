bd844b47b58cd247ed1899eb1a9ad74fe5a89424
Autovw <68228006+Autovw@users.noreply.github.com>
2021-06-30 14:16:59 +0200

    Added/updated new ArmorItem classes

---
+++ src/main/java/com/autovw/advancednetherite/content/UltimateArmorItem.java
@@ -0,0 +1,26 @@
+package com.autovw.advancednetherite.content;
+
+import com.autovw.advancednetherite.config.Config;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.monster.EndermanEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.IArmorMaterial;
+import net.minecraft.item.ItemStack;
+
+public class UltimateArmorItem extends ArmorItem {
+    public UltimateArmorItem(IArmorMaterial material, EquipmentSlotType slot, Properties builder) {
+        super(material, slot, builder);
+    }
+
+    @Override
+    public boolean makesPiglinsNeutral(ItemStack stack, LivingEntity wearer) {
+        return Config.ArmorConfig.diamondUltimatePassiveArmor.get();
+    }
+
+    @Override
+    public boolean isEnderMask(ItemStack stack, PlayerEntity player, EndermanEntity enderman) {
+        return Config.ArmorConfig.diamondUltimatePassiveArmor.get();
+    }
+}
