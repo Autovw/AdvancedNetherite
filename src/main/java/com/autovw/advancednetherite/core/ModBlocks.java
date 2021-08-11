54f7d9a914f8402723192a5d3e6f80c7d47a766d
Autovw <68228006+Autovw@users.noreply.github.com>
2021-08-11 10:42:15 +0200

    🔥 Blocks no longer utilize ToolType to prevent crash on Forge 37.0.31 and above

--- src/main/java/com/autovw/advancednetherite/core/ModBlocks.java
+++ src/main/java/com/autovw/advancednetherite/core/ModBlocks.java
@@ -8,7 +8,6 @@ import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.level.material.MaterialColor;
-import net.minecraftforge.common.ToolType;
 import net.minecraftforge.fmllegacy.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -23,34 +22,26 @@ public class ModBlocks {
     // BLOCKS
     // Netherite Blocks
     public static final RegistryObject<Block> NETHERITE_IRON_BLOCK = BLOCKS.register("netherite_iron_block",
-            () -> new Block(BlockBehaviour.Properties.of(Material.METAL, MaterialColor.COLOR_BLACK).harvestLevel(3)
-                    .harvestTool(ToolType.PICKAXE).requiresCorrectToolForDrops()
+            () -> new Block(BlockBehaviour.Properties.of(Material.METAL, MaterialColor.COLOR_BLACK).requiresCorrectToolForDrops()
                     .strength(50.0F, 1200.0F).sound(SoundType.NETHERITE_BLOCK)));
     public static final RegistryObject<Block> NETHERITE_GOLD_BLOCK = BLOCKS.register("netherite_gold_block",
-            () -> new Block(BlockBehaviour.Properties.of(Material.METAL, MaterialColor.COLOR_BLACK).harvestLevel(3)
-                    .harvestTool(ToolType.PICKAXE).requiresCorrectToolForDrops()
+            () -> new Block(BlockBehaviour.Properties.of(Material.METAL, MaterialColor.COLOR_BLACK).requiresCorrectToolForDrops()
                     .strength(50.0F, 1200.0F).sound(SoundType.NETHERITE_BLOCK)));
     public static final RegistryObject<Block> NETHERITE_EMERALD_BLOCK = BLOCKS.register("netherite_emerald_block",
-            () -> new Block(BlockBehaviour.Properties.of(Material.METAL, MaterialColor.COLOR_BLACK).harvestLevel(3)
-                    .harvestTool(ToolType.PICKAXE).requiresCorrectToolForDrops()
+            () -> new Block(BlockBehaviour.Properties.of(Material.METAL, MaterialColor.COLOR_BLACK).requiresCorrectToolForDrops()
                     .strength(50.0F, 1200.0F).sound(SoundType.NETHERITE_BLOCK)));
     public static final RegistryObject<Block> NETHERITE_DIAMOND_BLOCK = BLOCKS.register("netherite_diamond_block",
-            () -> new Block(BlockBehaviour.Properties.of(Material.METAL, MaterialColor.COLOR_BLACK).harvestLevel(3)
-                    .harvestTool(ToolType.PICKAXE).requiresCorrectToolForDrops()
+            () -> new Block(BlockBehaviour.Properties.of(Material.METAL, MaterialColor.COLOR_BLACK).requiresCorrectToolForDrops()
                     .strength(50.0F, 1200.0F).sound(SoundType.NETHERITE_BLOCK)));
 
     // BLOCK ITEMS
     // Netherite Blocks
     public static final RegistryObject<BlockItem> NETHERITE_IRON_BLOCKITEM = ModItems.ITEMS.register("netherite_iron_block",
-            () -> new BlockItem(ModBlocks.NETHERITE_IRON_BLOCK.get(), new Item.Properties().tab(Reference.TAB_ADVANCEDNETHERITE)
-                    .fireResistant()));
+            () -> new BlockItem(ModBlocks.NETHERITE_IRON_BLOCK.get(), new Item.Properties().tab(Reference.TAB_ADVANCEDNETHERITE).fireResistant()));
     public static final RegistryObject<BlockItem> NETHERITE_GOLD_BLOCKITEM = ModItems.ITEMS.register("netherite_gold_block",
-            () -> new BlockItem(ModBlocks.NETHERITE_GOLD_BLOCK.get(), new Item.Properties().tab(Reference.TAB_ADVANCEDNETHERITE)
-                    .fireResistant()));
+            () -> new BlockItem(ModBlocks.NETHERITE_GOLD_BLOCK.get(), new Item.Properties().tab(Reference.TAB_ADVANCEDNETHERITE).fireResistant()));
     public static final RegistryObject<BlockItem> NETHERITE_EMERALD_BLOCKITEM = ModItems.ITEMS.register("netherite_emerald_block",
-            () -> new BlockItem(ModBlocks.NETHERITE_EMERALD_BLOCK.get(), new Item.Properties().tab(Reference.TAB_ADVANCEDNETHERITE)
-                    .fireResistant()));
+            () -> new BlockItem(ModBlocks.NETHERITE_EMERALD_BLOCK.get(), new Item.Properties().tab(Reference.TAB_ADVANCEDNETHERITE).fireResistant()));
     public static final RegistryObject<BlockItem> NETHERITE_DIAMOND_BLOCKITEM = ModItems.ITEMS.register("netherite_diamond_block",
-            () -> new BlockItem(ModBlocks.NETHERITE_DIAMOND_BLOCK.get(), new Item.Properties().tab(Reference.TAB_ADVANCEDNETHERITE)
-                    .fireResistant()));
+            () -> new BlockItem(ModBlocks.NETHERITE_DIAMOND_BLOCK.get(), new Item.Properties().tab(Reference.TAB_ADVANCEDNETHERITE).fireResistant()));
 }
