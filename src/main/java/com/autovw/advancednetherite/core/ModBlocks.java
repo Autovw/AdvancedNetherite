b5823857da25e7a811990e28325c1ae81ab3fdd1
Autovw <68228006+Autovw@users.noreply.github.com>
2021-10-16 13:44:15 +0200

    ⚡ Item properties of blockitems are now set manually

--- src/main/java/com/autovw/advancednetherite/core/ModBlocks.java
+++ src/main/java/com/autovw/advancednetherite/core/ModBlocks.java
@@ -18,22 +18,22 @@ public class ModBlocks {
 
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, Reference.MOD_ID);
 
-    public static final RegistryObject<Block> NETHERITE_IRON_BLOCK = register("netherite_iron_block", Reference.TAB_ADVANCEDNETHERITE);
-    public static final RegistryObject<Block> NETHERITE_GOLD_BLOCK = register("netherite_gold_block", Reference.TAB_ADVANCEDNETHERITE);
-    public static final RegistryObject<Block> NETHERITE_EMERALD_BLOCK = register("netherite_emerald_block", Reference.TAB_ADVANCEDNETHERITE);
-    public static final RegistryObject<Block> NETHERITE_DIAMOND_BLOCK = register("netherite_diamond_block", Reference.TAB_ADVANCEDNETHERITE);
+    public static final RegistryObject<Block> NETHERITE_IRON_BLOCK = register("netherite_iron_block", new Item.Properties().tab(Reference.TAB_ADVANCEDNETHERITE).fireResistant());
+    public static final RegistryObject<Block> NETHERITE_GOLD_BLOCK = register("netherite_gold_block", new Item.Properties().tab(Reference.TAB_ADVANCEDNETHERITE).fireResistant());
+    public static final RegistryObject<Block> NETHERITE_EMERALD_BLOCK = register("netherite_emerald_block", new Item.Properties().tab(Reference.TAB_ADVANCEDNETHERITE).fireResistant());
+    public static final RegistryObject<Block> NETHERITE_DIAMOND_BLOCK = register("netherite_diamond_block", new Item.Properties().tab(Reference.TAB_ADVANCEDNETHERITE).fireResistant());
 
     /**
      * This method is used to automatically register blocks and blockitems.
      * The block properties are directly copied from the vanilla netherite block.
      *
      * @param name Name of the block
-     * @param creativeTab The creative tab for the blockitem
+     * @param properties The properties of the blockitem
      * @return The block + properties
      */
-    public static RegistryObject<Block> register(String name, CreativeModeTab creativeTab) {
+    public static RegistryObject<Block> register(String name, Item.Properties properties) {
         RegistryObject<Block> block = BLOCKS.register(name, () -> new Block(BlockBehaviour.Properties.copy(Blocks.NETHERITE_BLOCK)));
-        ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties().tab(creativeTab).fireResistant()));
+        ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), properties));
         return block;
     }
 }
