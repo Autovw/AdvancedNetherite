8eb429266e384d19b3babe68a8e49373926161ea
Autovw <68228006+Autovw@users.noreply.github.com>
2021-10-16 12:20:49 +0200

    ⚡ Improved block registry

--- src/main/java/com/autovw/advancednetherite/core/ModBlocks.java
+++ src/main/java/com/autovw/advancednetherite/core/ModBlocks.java
@@ -2,12 +2,11 @@ package com.autovw.advancednetherite.core;
 
 import com.autovw.advancednetherite.Reference;
 import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.Material;
-import net.minecraft.world.level.material.MaterialColor;
 import net.minecraftforge.fmllegacy.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -19,29 +18,22 @@ public class ModBlocks {
 
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, Reference.MOD_ID);
 
-    // BLOCKS
-    // Netherite Blocks
-    public static final RegistryObject<Block> NETHERITE_IRON_BLOCK = BLOCKS.register("netherite_iron_block",
-            () -> new Block(BlockBehaviour.Properties.of(Material.METAL, MaterialColor.COLOR_BLACK).requiresCorrectToolForDrops()
-                    .strength(50.0F, 1200.0F).sound(SoundType.NETHERITE_BLOCK)));
-    public static final RegistryObject<Block> NETHERITE_GOLD_BLOCK = BLOCKS.register("netherite_gold_block",
-            () -> new Block(BlockBehaviour.Properties.of(Material.METAL, MaterialColor.COLOR_BLACK).requiresCorrectToolForDrops()
-                    .strength(50.0F, 1200.0F).sound(SoundType.NETHERITE_BLOCK)));
-    public static final RegistryObject<Block> NETHERITE_EMERALD_BLOCK = BLOCKS.register("netherite_emerald_block",
-            () -> new Block(BlockBehaviour.Properties.of(Material.METAL, MaterialColor.COLOR_BLACK).requiresCorrectToolForDrops()
-                    .strength(50.0F, 1200.0F).sound(SoundType.NETHERITE_BLOCK)));
-    public static final RegistryObject<Block> NETHERITE_DIAMOND_BLOCK = BLOCKS.register("netherite_diamond_block",
-            () -> new Block(BlockBehaviour.Properties.of(Material.METAL, MaterialColor.COLOR_BLACK).requiresCorrectToolForDrops()
-                    .strength(50.0F, 1200.0F).sound(SoundType.NETHERITE_BLOCK)));
+    public static final RegistryObject<Block> NETHERITE_IRON_BLOCK = register("netherite_iron_block", Reference.TAB_ADVANCEDNETHERITE);
+    public static final RegistryObject<Block> NETHERITE_GOLD_BLOCK = register("netherite_gold_block", Reference.TAB_ADVANCEDNETHERITE);
+    public static final RegistryObject<Block> NETHERITE_EMERALD_BLOCK = register("netherite_emerald_block", Reference.TAB_ADVANCEDNETHERITE);
+    public static final RegistryObject<Block> NETHERITE_DIAMOND_BLOCK = register("netherite_diamond_block", Reference.TAB_ADVANCEDNETHERITE);
 
-    // BLOCK ITEMS
-    // Netherite Blocks
-    public static final RegistryObject<BlockItem> NETHERITE_IRON_BLOCKITEM = ModItems.ITEMS.register("netherite_iron_block",
-            () -> new BlockItem(ModBlocks.NETHERITE_IRON_BLOCK.get(), new Item.Properties().tab(Reference.TAB_ADVANCEDNETHERITE).fireResistant()));
-    public static final RegistryObject<BlockItem> NETHERITE_GOLD_BLOCKITEM = ModItems.ITEMS.register("netherite_gold_block",
-            () -> new BlockItem(ModBlocks.NETHERITE_GOLD_BLOCK.get(), new Item.Properties().tab(Reference.TAB_ADVANCEDNETHERITE).fireResistant()));
-    public static final RegistryObject<BlockItem> NETHERITE_EMERALD_BLOCKITEM = ModItems.ITEMS.register("netherite_emerald_block",
-            () -> new BlockItem(ModBlocks.NETHERITE_EMERALD_BLOCK.get(), new Item.Properties().tab(Reference.TAB_ADVANCEDNETHERITE).fireResistant()));
-    public static final RegistryObject<BlockItem> NETHERITE_DIAMOND_BLOCKITEM = ModItems.ITEMS.register("netherite_diamond_block",
-            () -> new BlockItem(ModBlocks.NETHERITE_DIAMOND_BLOCK.get(), new Item.Properties().tab(Reference.TAB_ADVANCEDNETHERITE).fireResistant()));
+    /**
+     * This method is used to automatically register blocks and blockitems.
+     * The block properties are directly copied from the vanilla netherite block.
+     *
+     * @param name Name of the block
+     * @param creativeTab The creative tab for the blockitem
+     * @return The block + properties
+     */
+    public static RegistryObject<Block> register(String name, CreativeModeTab creativeTab) {
+        RegistryObject<Block> block = BLOCKS.register(name, () -> new Block(BlockBehaviour.Properties.copy(Blocks.NETHERITE_BLOCK)));
+        ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties().tab(creativeTab).fireResistant()));
+        return block;
+    }
 }
