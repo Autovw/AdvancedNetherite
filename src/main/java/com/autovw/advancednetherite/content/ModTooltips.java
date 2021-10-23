a44c4c19d71e6885b0186606220b0671b567f245
Autovw <68228006+Autovw@users.noreply.github.com>
2021-10-23 10:58:22 +0200

    Revert "🎨The +'s on the tooltips are now displayed 'bold'"

--- src/main/java/com/autovw/advancednetherite/content/ModTooltips.java
+++ src/main/java/com/autovw/advancednetherite/content/ModTooltips.java
@@ -11,10 +11,8 @@ import net.minecraft.network.chat.TranslatableComponent;
 public class ModTooltips {
 
     // Endermask Tooltip
-    public static final TranslatableComponent endermaskText = AdvancedNetheriteAPI.tooltip(Reference.MOD_ID, "endermask_armor.perk_one", ChatFormatting.DARK_GREEN);
-    public static final TranslatableComponent endermaskTooltip = AdvancedNetheriteAPI.appendingTooltip(Reference.MOD_ID, "endermask_armor.plus", ChatFormatting.DARK_GREEN, endermaskText);
+    public static final TranslatableComponent endermaskTooltip = AdvancedNetheriteAPI.tooltip(Reference.MOD_ID, "endermask_armor.perk_one", ChatFormatting.DARK_GREEN);
 
     // Piglin Passive Tooltip
-    public static final TranslatableComponent piglinPassiveText = AdvancedNetheriteAPI.tooltip(Reference.MOD_ID, "piglinpassive_armor.perk_one", ChatFormatting.GOLD);
-    public static final TranslatableComponent piglinPassiveTooltip = AdvancedNetheriteAPI.appendingTooltip(Reference.MOD_ID, "piglinpassive_armor.plus", ChatFormatting.GOLD, piglinPassiveText);
+    public static final TranslatableComponent piglinPassiveTooltip = AdvancedNetheriteAPI.tooltip(Reference.MOD_ID, "piglinpassive_armor.perk_one", ChatFormatting.GOLD);
 }
