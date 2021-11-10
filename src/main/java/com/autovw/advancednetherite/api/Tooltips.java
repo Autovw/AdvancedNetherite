a447b6fc3ecb4345f18152d70ea402aade1adfcb
Autovw <68228006+Autovw@users.noreply.github.com>
2021-11-10 17:12:31 +0100

    ⚡ Changed javadoc

--- src/main/java/com/autovw/advancednetherite/api/Tooltips.java
+++ src/main/java/com/autovw/advancednetherite/api/Tooltips.java
@@ -6,7 +6,7 @@ import net.minecraft.network.chat.TranslatableComponent;
 /**
  * Author: Autovw
  * <br>
- * The API is a work in progress, not final!
+ * Added as of version 1.9.0, the API will be expanded over time.
  */
 public class Tooltips {
 
