944295a01ac42aaca2153677586a7f1e47b30b0b
Autovw <68228006+Autovw@users.noreply.github.com>
2022-01-04 12:07:20 +0100

    ✨ Created new Tool related classes for add-on use

--- src/main/java/com/autovw/advancednetherite/core/ModItems.java
+++ src/main/java/com/autovw/advancednetherite/core/ModItems.java
@@ -1,7 +1,7 @@
 package com.autovw.advancednetherite.core;
 
 import com.autovw.advancednetherite.Reference;
-import com.autovw.advancednetherite.common.item.AdvancedArmorItem;
+import com.autovw.advancednetherite.common.item.*;
 import com.autovw.advancednetherite.content.armor.*;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.*;
@@ -47,36 +47,36 @@ public class ModItems {
     public static final RegistryObject<Item> NETHERITE_DIAMOND_LEGGINGS = register("netherite_diamond_leggings", new NetheriteDiamondArmorItem(ModArmorTiers.NETHERITE_DIAMOND, EquipmentSlot.LEGS, (new Item.Properties().tab(Reference.TAB_ADVANCEDNETHERITE))));
     public static final RegistryObject<Item> NETHERITE_DIAMOND_BOOTS = register("netherite_diamond_boots", new NetheriteDiamondArmorItem(ModArmorTiers.NETHERITE_DIAMOND, EquipmentSlot.FEET, (new Item.Properties().tab(Reference.TAB_ADVANCEDNETHERITE))));
 
-    // TOOLS (int = attackDamageIn, float = attackSpeedIn)
+    // TOOLS
     // Axes
-    public static final RegistryObject<Item> NETHERITE_IRON_AXE = register("netherite_iron_axe", new AxeItem(ModToolTiers.NETHERITE_IRON, 5, -3.0f, (new Item.Properties().tab(Reference.TAB_ADVANCEDNETHERITE).fireResistant())));
-    public static final RegistryObject<Item> NETHERITE_GOLD_AXE = register("netherite_gold_axe", new AxeItem(ModToolTiers.NETHERITE_GOLD, 6, -3.0f, (new Item.Properties().tab(Reference.TAB_ADVANCEDNETHERITE).fireResistant())));
-    public static final RegistryObject<Item> NETHERITE_EMERALD_AXE = register("netherite_emerald_axe", new AxeItem(ModToolTiers.NETHERITE_EMERALD, 6, -3.0f, (new Item.Properties().tab(Reference.TAB_ADVANCEDNETHERITE).fireResistant())));
-    public static final RegistryObject<Item> NETHERITE_DIAMOND_AXE = register("netherite_diamond_axe", new AxeItem(ModToolTiers.NETHERITE_DIAMOND, 7, -3.0f, (new Item.Properties().tab(Reference.TAB_ADVANCEDNETHERITE).fireResistant())));
+    public static final RegistryObject<Item> NETHERITE_IRON_AXE = register("netherite_iron_axe", new AdvancedAxeItem(ModToolTiers.NETHERITE_IRON, 5, -3.0f, (new Item.Properties().tab(Reference.TAB_ADVANCEDNETHERITE))));
+    public static final RegistryObject<Item> NETHERITE_GOLD_AXE = register("netherite_gold_axe", new AdvancedAxeItem(ModToolTiers.NETHERITE_GOLD, 6, -3.0f, (new Item.Properties().tab(Reference.TAB_ADVANCEDNETHERITE))));
+    public static final RegistryObject<Item> NETHERITE_EMERALD_AXE = register("netherite_emerald_axe", new AdvancedAxeItem(ModToolTiers.NETHERITE_EMERALD, 6, -3.0f, (new Item.Properties().tab(Reference.TAB_ADVANCEDNETHERITE))));
+    public static final RegistryObject<Item> NETHERITE_DIAMOND_AXE = register("netherite_diamond_axe", new AdvancedAxeItem(ModToolTiers.NETHERITE_DIAMOND, 7, -3.0f, (new Item.Properties().tab(Reference.TAB_ADVANCEDNETHERITE))));
 
     // Hoes
-    public static final RegistryObject<Item> NETHERITE_IRON_HOE = register("netherite_iron_hoe", new HoeItem(ModToolTiers.NETHERITE_IRON, -4, 0.0F, (new Item.Properties().tab(Reference.TAB_ADVANCEDNETHERITE).fireResistant())));
-    public static final RegistryObject<Item> NETHERITE_GOLD_HOE = register("netherite_gold_hoe", new HoeItem(ModToolTiers.NETHERITE_GOLD, -4, 0.0F, (new Item.Properties().tab(Reference.TAB_ADVANCEDNETHERITE).fireResistant())));
-    public static final RegistryObject<Item> NETHERITE_EMERALD_HOE = register("netherite_emerald_hoe", new HoeItem(ModToolTiers.NETHERITE_EMERALD, -5, 0.0F, (new Item.Properties().tab(Reference.TAB_ADVANCEDNETHERITE).fireResistant())));
-    public static final RegistryObject<Item> NETHERITE_DIAMOND_HOE = register("netherite_diamond_hoe", new HoeItem(ModToolTiers.NETHERITE_DIAMOND, -5, 0.0F, (new Item.Properties().tab(Reference.TAB_ADVANCEDNETHERITE).fireResistant())));
+    public static final RegistryObject<Item> NETHERITE_IRON_HOE = register("netherite_iron_hoe", new AdvancedHoeItem(ModToolTiers.NETHERITE_IRON, -4, 0.0F, (new Item.Properties().tab(Reference.TAB_ADVANCEDNETHERITE))));
+    public static final RegistryObject<Item> NETHERITE_GOLD_HOE = register("netherite_gold_hoe", new AdvancedHoeItem(ModToolTiers.NETHERITE_GOLD, -4, 0.0F, (new Item.Properties().tab(Reference.TAB_ADVANCEDNETHERITE))));
+    public static final RegistryObject<Item> NETHERITE_EMERALD_HOE = register("netherite_emerald_hoe", new AdvancedHoeItem(ModToolTiers.NETHERITE_EMERALD, -5, 0.0F, (new Item.Properties().tab(Reference.TAB_ADVANCEDNETHERITE))));
+    public static final RegistryObject<Item> NETHERITE_DIAMOND_HOE = register("netherite_diamond_hoe", new AdvancedHoeItem(ModToolTiers.NETHERITE_DIAMOND, -5, 0.0F, (new Item.Properties().tab(Reference.TAB_ADVANCEDNETHERITE))));
 
     // Pickaxes
-    public static final RegistryObject<Item> NETHERITE_IRON_PICKAXE = register("netherite_iron_pickaxe", new PickaxeItem(ModToolTiers.NETHERITE_IRON, 1, -2.8F, (new Item.Properties().tab(Reference.TAB_ADVANCEDNETHERITE).fireResistant())));
-    public static final RegistryObject<Item> NETHERITE_GOLD_PICKAXE = register("netherite_gold_pickaxe", new PickaxeItem(ModToolTiers.NETHERITE_GOLD, 1, -2.8F, (new Item.Properties().tab(Reference.TAB_ADVANCEDNETHERITE).fireResistant())));
-    public static final RegistryObject<Item> NETHERITE_EMERALD_PICKAXE = register("netherite_emerald_pickaxe", new PickaxeItem(ModToolTiers.NETHERITE_EMERALD, 1, -2.8F, (new Item.Properties().tab(Reference.TAB_ADVANCEDNETHERITE).fireResistant())));
-    public static final RegistryObject<Item> NETHERITE_DIAMOND_PICKAXE = register("netherite_diamond_pickaxe", new PickaxeItem(ModToolTiers.NETHERITE_DIAMOND, 1, -2.8F, (new Item.Properties().tab(Reference.TAB_ADVANCEDNETHERITE).fireResistant())));
+    public static final RegistryObject<Item> NETHERITE_IRON_PICKAXE = register("netherite_iron_pickaxe", new AdvancedPickaxeItem(ModToolTiers.NETHERITE_IRON, 1, -2.8F, (new Item.Properties().tab(Reference.TAB_ADVANCEDNETHERITE))));
+    public static final RegistryObject<Item> NETHERITE_GOLD_PICKAXE = register("netherite_gold_pickaxe", new AdvancedPickaxeItem(ModToolTiers.NETHERITE_GOLD, 1, -2.8F, (new Item.Properties().tab(Reference.TAB_ADVANCEDNETHERITE))));
+    public static final RegistryObject<Item> NETHERITE_EMERALD_PICKAXE = register("netherite_emerald_pickaxe", new AdvancedPickaxeItem(ModToolTiers.NETHERITE_EMERALD, 1, -2.8F, (new Item.Properties().tab(Reference.TAB_ADVANCEDNETHERITE))));
+    public static final RegistryObject<Item> NETHERITE_DIAMOND_PICKAXE = register("netherite_diamond_pickaxe", new AdvancedPickaxeItem(ModToolTiers.NETHERITE_DIAMOND, 1, -2.8F, (new Item.Properties().tab(Reference.TAB_ADVANCEDNETHERITE))));
 
     // Shovels
-    public static final RegistryObject<Item> NETHERITE_IRON_SHOVEL = register("netherite_iron_shovel", new ShovelItem(ModToolTiers.NETHERITE_IRON, 1.5F, -3.0F, (new Item.Properties().tab(Reference.TAB_ADVANCEDNETHERITE).fireResistant())));
-    public static final RegistryObject<Item> NETHERITE_GOLD_SHOVEL = register("netherite_gold_shovel", new ShovelItem(ModToolTiers.NETHERITE_GOLD, 1.5F, -3.0F, (new Item.Properties().tab(Reference.TAB_ADVANCEDNETHERITE).fireResistant())));
-    public static final RegistryObject<Item> NETHERITE_EMERALD_SHOVEL = register("netherite_emerald_shovel", new ShovelItem(ModToolTiers.NETHERITE_EMERALD, 1, -3.0F, (new Item.Properties().tab(Reference.TAB_ADVANCEDNETHERITE).fireResistant())));
-    public static final RegistryObject<Item> NETHERITE_DIAMOND_SHOVEL = register("netherite_diamond_shovel", new ShovelItem(ModToolTiers.NETHERITE_DIAMOND, 1, -3.0F, (new Item.Properties().tab(Reference.TAB_ADVANCEDNETHERITE).fireResistant())));
+    public static final RegistryObject<Item> NETHERITE_IRON_SHOVEL = register("netherite_iron_shovel", new AdvancedShovelItem(ModToolTiers.NETHERITE_IRON, 1.5F, -3.0F, (new Item.Properties().tab(Reference.TAB_ADVANCEDNETHERITE))));
+    public static final RegistryObject<Item> NETHERITE_GOLD_SHOVEL = register("netherite_gold_shovel", new AdvancedShovelItem(ModToolTiers.NETHERITE_GOLD, 1.5F, -3.0F, (new Item.Properties().tab(Reference.TAB_ADVANCEDNETHERITE))));
+    public static final RegistryObject<Item> NETHERITE_EMERALD_SHOVEL = register("netherite_emerald_shovel", new AdvancedShovelItem(ModToolTiers.NETHERITE_EMERALD, 1, -3.0F, (new Item.Properties().tab(Reference.TAB_ADVANCEDNETHERITE))));
+    public static final RegistryObject<Item> NETHERITE_DIAMOND_SHOVEL = register("netherite_diamond_shovel", new AdvancedShovelItem(ModToolTiers.NETHERITE_DIAMOND, 1, -3.0F, (new Item.Properties().tab(Reference.TAB_ADVANCEDNETHERITE))));
 
     // Swords
-    public static final RegistryObject<Item> NETHERITE_IRON_SWORD = register("netherite_iron_sword", new SwordItem(ModToolTiers.NETHERITE_IRON, 3, -2.4F, (new Item.Properties().tab(Reference.TAB_ADVANCEDNETHERITE).fireResistant())));
-    public static final RegistryObject<Item> NETHERITE_GOLD_SWORD = register("netherite_gold_sword", new SwordItem(ModToolTiers.NETHERITE_GOLD, 4, -2.4F, (new Item.Properties().tab(Reference.TAB_ADVANCEDNETHERITE).fireResistant())));
-    public static final RegistryObject<Item> NETHERITE_EMERALD_SWORD = register("netherite_emerald_sword", new SwordItem(ModToolTiers.NETHERITE_EMERALD, 4, -2.4F, (new Item.Properties().tab(Reference.TAB_ADVANCEDNETHERITE).fireResistant())));
-    public static final RegistryObject<Item> NETHERITE_DIAMOND_SWORD = register("netherite_diamond_sword", new SwordItem(ModToolTiers.NETHERITE_DIAMOND, 5, -2.4F, (new Item.Properties().tab(Reference.TAB_ADVANCEDNETHERITE).fireResistant())));
+    public static final RegistryObject<Item> NETHERITE_IRON_SWORD = register("netherite_iron_sword", new AdvancedSwordItem(ModToolTiers.NETHERITE_IRON, 3, -2.4F, (new Item.Properties().tab(Reference.TAB_ADVANCEDNETHERITE))));
+    public static final RegistryObject<Item> NETHERITE_GOLD_SWORD = register("netherite_gold_sword", new AdvancedSwordItem(ModToolTiers.NETHERITE_GOLD, 4, -2.4F, (new Item.Properties().tab(Reference.TAB_ADVANCEDNETHERITE))));
+    public static final RegistryObject<Item> NETHERITE_EMERALD_SWORD = register("netherite_emerald_sword", new AdvancedSwordItem(ModToolTiers.NETHERITE_EMERALD, 4, -2.4F, (new Item.Properties().tab(Reference.TAB_ADVANCEDNETHERITE))));
+    public static final RegistryObject<Item> NETHERITE_DIAMOND_SWORD = register("netherite_diamond_sword", new AdvancedSwordItem(ModToolTiers.NETHERITE_DIAMOND, 5, -2.4F, (new Item.Properties().tab(Reference.TAB_ADVANCEDNETHERITE))));
 
     // Helper method used to register items
     private static RegistryObject<Item> register(String name, Item item) {
