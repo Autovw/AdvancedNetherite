e70f8ae3e5fe1f5f997992981ac3736a51f171e2
Autovw <68228006+Autovw@users.noreply.github.com>
2022-02-14 20:09:47 +0100

    ✨ Added a client-side option to enable a search bar in the creative tab

--- src/main/java/com/autovw/advancednetherite/AdvancedNetheriteTab.java
+++ src/main/java/com/autovw/advancednetherite/AdvancedNetheriteTab.java
@@ -1,7 +1,10 @@
 package com.autovw.advancednetherite;
 
+import com.autovw.advancednetherite.config.Config;
 import com.autovw.advancednetherite.core.ModItems;
 import net.minecraft.core.NonNullList;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 
@@ -23,5 +26,30 @@ public class AdvancedNetheriteTab extends CreativeModeTab {
     @Override
     public void fillItemList(NonNullList<ItemStack> items) {
         super.fillItemList(items);
+    }
+
+    /**
+     * Client-side feature which enables a search bar inside the Advanced Netherite creative tab.
+     * This is turned off by default.
+     */
+    @Override
+    public boolean hasSearchBar() {
+        return Config.Client.enableSearchBarInCreativeTab.get();
+    }
+
+    /**
+     * Removes the title of the creative tab if the search bar is present
+     */
+    @Override
+    public boolean showTitle() {
+        return !hasSearchBar();
+    }
+
+    /**
+     * Sets the background texture of the creative tab to the vanilla background texture of the search tab to allow for compatibility with resource packs!
+     */
+    @Override
+    public ResourceLocation getBackgroundImage() {
+        return hasSearchBar() ? new ResourceLocation("textures/gui/container/creative_inventory/tab_item_search.png") : super.getBackgroundImage();
     }
 }
