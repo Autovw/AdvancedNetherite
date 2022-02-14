03b555b803c8858f5ceb580ee795c2a5278c51d3
Autovw <68228006+Autovw@users.noreply.github.com>
2022-02-14 20:15:41 +0100

    ✨ Added ``getBarColor()`` to ``AdvancedItem``

--- src/main/java/com/autovw/advancednetherite/common/item/AdvancedItem.java
+++ src/main/java/com/autovw/advancednetherite/common/item/AdvancedItem.java
@@ -1,6 +1,7 @@
 package com.autovw.advancednetherite.common.item;
 
 import com.autovw.advancednetherite.config.Config;
+import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -9,6 +10,7 @@ import net.minecraft.world.level.Level;
 
 import javax.annotation.Nullable;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Author: Autovw
@@ -54,5 +56,24 @@ public class AdvancedItem extends Item {
         if (Config.Client.showTooltips.get()) {
             addTooltips(stack, tooltip, flag);
         }
+    }
+
+    /**
+     * {@link Override} this method if you want to give your item a custom durability bar color.
+     * Feature is disabled by default, can be enabled in Advanced Netherite's Client config.
+     *
+     * @param stack The item stack
+     * @return The custom durability bar color
+     */
+    public ChatFormatting customDurabilityBarColor(ItemStack stack) {
+        return null;
+    }
+
+    /**
+     * Don't override this method, use {@link AdvancedItem#customDurabilityBarColor(ItemStack)} to change the custom durability bar color.
+     */
+    @Override
+    public int getBarColor(ItemStack stack) {
+        return customDurabilityBarColor(stack) != null && Config.Client.matchingDurabilityBars.get() ? Objects.requireNonNull(customDurabilityBarColor(stack).getColor()) : super.getBarColor(stack);
     }
 }
