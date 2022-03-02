428d32dfa6e233ade65fa98c9aecedb7597a0e33
Autovw <68228006+Autovw@users.noreply.github.com>
2022-03-02 12:28:10 +0100

    ⚡ Moved to slf4j

--- src/main/java/com/autovw/advancednetherite/AdvancedNetherite.java
+++ src/main/java/com/autovw/advancednetherite/AdvancedNetherite.java
@@ -4,6 +4,7 @@ import com.autovw.advancednetherite.config.Config;
 import com.autovw.advancednetherite.core.ModBlocks;
 import com.autovw.advancednetherite.core.ModItems;
 import com.autovw.advancednetherite.core.ModToolTiers;
+import com.mojang.logging.LogUtils;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ModLoadingContext;
@@ -11,15 +12,14 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
 
 /**
  * Author: Autovw
  */
 @Mod(Reference.MOD_ID)
 public class AdvancedNetherite {
-    public static final Logger LOGGER = LogManager.getLogger();
+    public static final Logger LOGGER = LogUtils.getLogger();
 
     public AdvancedNetherite() {
         ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, Config.clientConfig);
