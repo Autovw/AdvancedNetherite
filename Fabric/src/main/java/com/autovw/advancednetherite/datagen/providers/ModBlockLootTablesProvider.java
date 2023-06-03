a3bf431c5c4109d9aff8b912652c9713d8b67a80
Autovw <68228006+Autovw@users.noreply.github.com>
2023-06-03 14:59:30 +0200

    ⚡ Remove erroring Fabric code

--- Fabric/src/main/java/com/autovw/advancednetherite/datagen/providers/ModBlockLootTablesProvider.java
+++ Fabric/src/main/java/com/autovw/advancednetherite/datagen/providers/ModBlockLootTablesProvider.java
@@ -3,10 +3,6 @@ package com.autovw.advancednetherite.datagen.providers;
 import com.autovw.advancednetherite.core.ModBlocks;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.level.storage.loot.LootTable;
-
-import java.util.function.BiConsumer;
 
 /**
  * @author Autovw
@@ -25,10 +21,5 @@ public class ModBlockLootTablesProvider extends FabricBlockLootTableProvider
         this.dropSelf(ModBlocks.NETHERITE_GOLD_BLOCK);
         this.dropSelf(ModBlocks.NETHERITE_EMERALD_BLOCK);
         this.dropSelf(ModBlocks.NETHERITE_DIAMOND_BLOCK);
-    }
-
-    @Override
-    public void accept(BiConsumer<ResourceLocation, LootTable.Builder> resourceLocationBuilderBiConsumer)
-    {
     }
 }
