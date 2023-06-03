f2e95f7399ed35cac28fa5650962d5bf59978194
Autovw <68228006+Autovw@users.noreply.github.com>
2023-06-03 09:43:32 +0200

    ✨ Create Fabric recipe generator

--- Fabric/src/main/java/com/autovw/advancednetherite/datagen/ModDataGenerator.java
+++ Fabric/src/main/java/com/autovw/advancednetherite/datagen/ModDataGenerator.java
@@ -1,9 +1,6 @@
 package com.autovw.advancednetherite.datagen;
 
-import com.autovw.advancednetherite.datagen.providers.ModBlockLootTablesProvider;
-import com.autovw.advancednetherite.datagen.providers.ModBlockStatesProvider;
-import com.autovw.advancednetherite.datagen.providers.ModBlockTagsProvider;
-import com.autovw.advancednetherite.datagen.providers.ModItemTagsProvider;
+import com.autovw.advancednetherite.datagen.providers.*;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
@@ -19,6 +16,7 @@ public class ModDataGenerator implements DataGeneratorEntrypoint
 
         pack.addProvider(ModBlockStatesProvider::new);
         pack.addProvider(ModBlockLootTablesProvider::new);
+        pack.addProvider(ModRecipeProvider::new);
 
         pack.addProvider(ModBlockTagsProvider::new);
         pack.addProvider(ModItemTagsProvider::new);
