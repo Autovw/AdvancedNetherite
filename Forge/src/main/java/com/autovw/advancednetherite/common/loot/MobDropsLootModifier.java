6668e5aeec0759606d0cdff1eebe1753898659f4
Autovw <68228006+Autovw@users.noreply.github.com>
2023-06-11 15:59:04 +0200

    ⚡ Make loot modifiers use the config helper instead (Forge)

--- Forge/src/main/java/com/autovw/advancednetherite/common/loot/MobDropsLootModifier.java
+++ Forge/src/main/java/com/autovw/advancednetherite/common/loot/MobDropsLootModifier.java
@@ -1,6 +1,6 @@
 package com.autovw.advancednetherite.common.loot;
 
-import com.autovw.advancednetherite.config.Config;
+import com.autovw.advancednetherite.config.ConfigHelper;
 import com.autovw.advancednetherite.core.ModItems;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
@@ -25,39 +25,46 @@ import org.jetbrains.annotations.NotNull;
  * @apiNote This loot modifier can be disabled by {@link com.autovw.advancednetherite.config.Config.AdditionalDropsConfig#enableAdditionalMobDrops}
  * @author Autovw
  */
-public class MobDropsLootModifier extends LootModifier {
-    public static final Codec<MobDropsLootModifier> CODEC = RecordCodecBuilder.create(instance -> codecStart(instance)
-            .apply(instance, MobDropsLootModifier::new));
+public class MobDropsLootModifier extends LootModifier
+{
+    public static final Codec<MobDropsLootModifier> CODEC = RecordCodecBuilder.create(instance -> codecStart(instance).apply(instance, MobDropsLootModifier::new));
 
     /**
      * Constructs a LootModifier.
      *
      * @param conditionsIn the ILootConditions that need to be matched before the loot is modified.
      */
-    public MobDropsLootModifier(LootItemCondition[] conditionsIn) {
+    public MobDropsLootModifier(LootItemCondition[] conditionsIn)
+    {
         super(conditionsIn);
     }
 
     @NotNull
     @Override
-    protected ObjectArrayList<ItemStack> doApply(ObjectArrayList<ItemStack> generatedLoot, LootContext context) {
+    protected ObjectArrayList<ItemStack> doApply(ObjectArrayList<ItemStack> generatedLoot, LootContext context)
+    {
         Entity killer = context.getParamOrNull(LootContextParams.KILLER_ENTITY); // the entity killer
         Entity victim = context.getParamOrNull(LootContextParams.THIS_ENTITY); // killed entity
 
-        if (killer instanceof Player player && victim != null && Config.AdditionalDropsConfig.enableAdditionalMobDrops.get()) {
+        if (killer instanceof Player player && victim != null && ConfigHelper.get().getCommon().getAdditionalDrops().enableAdditionalMobDrops())
+        {
             ItemStack useItem = player.getMainHandItem(); // used to check if the player uses the correct weapon
             RandomSource random = context.getRandom(); // random generator
 
-            if (useItem.is(ModItems.NETHERITE_IRON_SWORD) && victim.getType() == EntityType.PHANTOM && random.nextFloat() <= Config.AdditionalDropProperties.additionalPhantomDropChance.get()) {
+            if (useItem.is(ModItems.NETHERITE_IRON_SWORD) && victim.getType() == EntityType.PHANTOM && random.nextFloat() <= ConfigHelper.get().getServer().getAdditionalDropProperties().getAdditionalPhantomDropChance())
+            {
                 generatedLoot.add(new ItemStack(Items.PHANTOM_MEMBRANE, random.nextIntBetweenInclusive(0, 2)));
             }
-            if ((useItem.is(ModItems.NETHERITE_GOLD_SWORD) || useItem.is(ModItems.NETHERITE_DIAMOND_SWORD)) && victim.getType() == EntityType.ZOMBIFIED_PIGLIN && random.nextFloat() <= Config.AdditionalDropProperties.additionalZombifiedPiglinDropChance.get()) {
+            if ((useItem.is(ModItems.NETHERITE_GOLD_SWORD) || useItem.is(ModItems.NETHERITE_DIAMOND_SWORD)) && victim.getType() == EntityType.ZOMBIFIED_PIGLIN && random.nextFloat() <= ConfigHelper.get().getServer().getAdditionalDropProperties().getAdditionalZombifiedPiglinDropChance())
+            {
                 generatedLoot.add(new ItemStack(Items.GOLD_NUGGET, random.nextIntBetweenInclusive(0, 3)));
             }
-            if ((useItem.is(ModItems.NETHERITE_GOLD_SWORD) || useItem.is(ModItems.NETHERITE_DIAMOND_SWORD)) && victim.getType() == EntityType.PIGLIN && random.nextFloat() <= Config.AdditionalDropProperties.additionalPiglinDropChance.get()) {
+            if ((useItem.is(ModItems.NETHERITE_GOLD_SWORD) || useItem.is(ModItems.NETHERITE_DIAMOND_SWORD)) && victim.getType() == EntityType.PIGLIN && random.nextFloat() <= ConfigHelper.get().getServer().getAdditionalDropProperties().getAdditionalPiglinDropChance())
+            {
                 generatedLoot.add(new ItemStack(Items.GOLD_INGOT, random.nextIntBetweenInclusive(1, 1)));
             }
-            if ((useItem.is(ModItems.NETHERITE_EMERALD_SWORD) || useItem.is(ModItems.NETHERITE_DIAMOND_SWORD)) && victim.getType() == EntityType.ENDERMAN && random.nextFloat() <= Config.AdditionalDropProperties.additionalEndermanDropChance.get()) {
+            if ((useItem.is(ModItems.NETHERITE_EMERALD_SWORD) || useItem.is(ModItems.NETHERITE_DIAMOND_SWORD)) && victim.getType() == EntityType.ENDERMAN && random.nextFloat() <= ConfigHelper.get().getServer().getAdditionalDropProperties().getAdditionalEndermanDropChance())
+            {
                 generatedLoot.add(new ItemStack(Items.ENDER_PEARL, random.nextIntBetweenInclusive(0, 1)));
             }
         }
@@ -65,7 +72,8 @@ public class MobDropsLootModifier extends LootModifier {
     }
 
     @Override
-    public Codec<? extends IGlobalLootModifier> codec() {
+    public Codec<? extends IGlobalLootModifier> codec()
+    {
         return CODEC;
     }
 }
--- Forge/src/main/java/com/autovw/advancednetherite/common/loot/OreDropsLootModifier.java
+++ Forge/src/main/java/com/autovw/advancednetherite/common/loot/OreDropsLootModifier.java
@@ -1,6 +1,6 @@
 package com.autovw.advancednetherite.common.loot;
 
-import com.autovw.advancednetherite.config.Config;
+import com.autovw.advancednetherite.config.ConfigHelper;
 import com.autovw.advancednetherite.core.ModItems;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
@@ -28,48 +28,57 @@ import org.jetbrains.annotations.NotNull;
  * @apiNote This loot modifier can be disabled by {@link com.autovw.advancednetherite.config.Config.AdditionalDropsConfig#enableAdditionalOreDrops}
  * @author Autovw
  */
-public class OreDropsLootModifier extends LootModifier {
-    public static final Codec<OreDropsLootModifier> CODEC = RecordCodecBuilder.create(instance -> codecStart(instance)
-            .apply(instance, OreDropsLootModifier::new));
+public class OreDropsLootModifier extends LootModifier
+{
+    public static final Codec<OreDropsLootModifier> CODEC = RecordCodecBuilder.create(instance -> codecStart(instance).apply(instance, OreDropsLootModifier::new));
 
     /**
      * Constructs a LootModifier.
      *
      * @param conditionsIn the ILootConditions that need to be matched before the loot is modified.
      */
-    public OreDropsLootModifier(LootItemCondition[] conditionsIn) {
+    public OreDropsLootModifier(LootItemCondition[] conditionsIn)
+    {
         super(conditionsIn);
     }
 
     @NotNull
     @Override
-    protected ObjectArrayList<ItemStack> doApply(ObjectArrayList<ItemStack> generatedLoot, LootContext context) {
+    protected ObjectArrayList<ItemStack> doApply(ObjectArrayList<ItemStack> generatedLoot, LootContext context)
+    {
         ItemStack tool = context.getParamOrNull(LootContextParams.TOOL);
         BlockState blockState = context.getParamOrNull(LootContextParams.BLOCK_STATE);
 
-        if (tool != null && blockState != null && Config.AdditionalDropsConfig.enableAdditionalOreDrops.get()) {
+        if (tool != null && blockState != null && ConfigHelper.get().getCommon().getAdditionalDrops().enableAdditionalOreDrops())
+        {
             Block block = blockState.getBlock();
             Item toolItem = tool.getItem();
             RandomSource random = context.getRandom();
 
-            if (EnchantmentHelper.getEnchantments(tool).containsKey(Enchantments.SILK_TOUCH)) {
+            if (EnchantmentHelper.getEnchantments(tool).containsKey(Enchantments.SILK_TOUCH))
+            {
                 return generatedLoot; // return early if tool is enchanted with silk touch
             }
 
-            if ((block == Blocks.IRON_ORE || block == Blocks.DEEPSLATE_IRON_ORE) && toolItem == ModItems.NETHERITE_IRON_PICKAXE && random.nextFloat() <= Config.AdditionalDropProperties.additionalRawIronDropChance.get()) {
+            if ((block == Blocks.IRON_ORE || block == Blocks.DEEPSLATE_IRON_ORE) && toolItem == ModItems.NETHERITE_IRON_PICKAXE && random.nextFloat() <= ConfigHelper.get().getServer().getAdditionalDropProperties().getAdditionalRawIronDropChance())
+            {
                 generatedLoot.add(new ItemStack(Items.RAW_IRON, random.nextIntBetweenInclusive(1, 2)));
             }
-            if ((block == Blocks.GOLD_ORE || block == Blocks.DEEPSLATE_GOLD_ORE) && toolItem == ModItems.NETHERITE_GOLD_PICKAXE && random.nextFloat() <= Config.AdditionalDropProperties.additionalRawGoldDropChance.get()) {
+            if ((block == Blocks.GOLD_ORE || block == Blocks.DEEPSLATE_GOLD_ORE) && toolItem == ModItems.NETHERITE_GOLD_PICKAXE && random.nextFloat() <= ConfigHelper.get().getServer().getAdditionalDropProperties().getAdditionalRawGoldDropChance())
+            {
                 generatedLoot.add(new ItemStack(Items.RAW_GOLD, random.nextIntBetweenInclusive(1, 1)));
             }
-            if ((block == Blocks.EMERALD_ORE || block == Blocks.DEEPSLATE_EMERALD_ORE) && toolItem == ModItems.NETHERITE_EMERALD_PICKAXE && random.nextFloat() <= Config.AdditionalDropProperties.additionalEmeraldDropChance.get()) {
+            if ((block == Blocks.EMERALD_ORE || block == Blocks.DEEPSLATE_EMERALD_ORE) && toolItem == ModItems.NETHERITE_EMERALD_PICKAXE && random.nextFloat() <= ConfigHelper.get().getServer().getAdditionalDropProperties().getAdditionalEmeraldDropChance())
+            {
                 generatedLoot.add(new ItemStack(Items.EMERALD, random.nextIntBetweenInclusive(1, 1)));
             }
-            if ((block == Blocks.DIAMOND_ORE || block == Blocks.DEEPSLATE_DIAMOND_ORE) && toolItem == ModItems.NETHERITE_DIAMOND_PICKAXE && random.nextFloat() <= Config.AdditionalDropProperties.additionalDiamondDropChance.get()) {
+            if ((block == Blocks.DIAMOND_ORE || block == Blocks.DEEPSLATE_DIAMOND_ORE) && toolItem == ModItems.NETHERITE_DIAMOND_PICKAXE && random.nextFloat() <= ConfigHelper.get().getServer().getAdditionalDropProperties().getAdditionalDiamondDropChance())
+            {
                 generatedLoot.add(new ItemStack(Items.DIAMOND, random.nextIntBetweenInclusive(1, 1)));
             }
 
-            if (block == Blocks.NETHER_GOLD_ORE && toolItem == ModItems.NETHERITE_GOLD_PICKAXE && random.nextFloat() <= Config.AdditionalDropProperties.additionalGoldNuggetDropChance.get()) {
+            if (block == Blocks.NETHER_GOLD_ORE && toolItem == ModItems.NETHERITE_GOLD_PICKAXE && random.nextFloat() <= ConfigHelper.get().getServer().getAdditionalDropProperties().getAdditionalGoldNuggetDropChance())
+            {
                 generatedLoot.add(new ItemStack(Items.GOLD_NUGGET, random.nextIntBetweenInclusive(1, 3)));
             }
         }
@@ -77,7 +86,8 @@ public class OreDropsLootModifier extends LootModifier {
     }
 
     @Override
-    public Codec<? extends IGlobalLootModifier> codec() {
+    public Codec<? extends IGlobalLootModifier> codec()
+    {
         return CODEC;
     }
 }
