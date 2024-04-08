f2d2f71b1d777c288746ec0e0dc0a0274a3bc9c1
Autovw <68228006+Autovw@users.noreply.github.com>
2024-04-08 16:47:26 +0200

    ⚡ Add more common tags (Fabric)

--- Fabric/src/main/java/com/autovw/advancednetherite/core/util/FabricModTags.java
+++ Fabric/src/main/java/com/autovw/advancednetherite/core/util/FabricModTags.java
@@ -4,6 +4,7 @@ import com.autovw.advancednetherite.AdvancedNetherite;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
 
 /**
  * Collection of Fabric only tags
@@ -16,7 +17,25 @@ public final class FabricModTags
     public static final TagKey<Item> CONFIG_PACIFY_PIGLINS = ModTags.itemTag(new ResourceLocation(AdvancedNetherite.MOD_ID, "config/pacify_piglins"));
     public static final TagKey<Item> CONFIG_PACIFY_ENDERMEN = ModTags.itemTag(new ResourceLocation(AdvancedNetherite.MOD_ID, "config/pacify_endermen"));
 
+    // "c" (common) tags
+    public static final TagKey<Item> COMMON_HELMETS = commonItemTag("helmets");
+    public static final TagKey<Item> COMMON_CHESTPLATES = commonItemTag("chestplates");
+    public static final TagKey<Item> COMMON_LEGGINGS = commonItemTag("leggings");
+    public static final TagKey<Item> COMMON_BOOTS = commonItemTag("boots");
+    public static final TagKey<Item> COMMON_NETHERITE_INGOTS = commonItemTag("netherite_ingots");
+    public static final TagKey<Block> COMMON_NETHERITE_BLOCKS = commonBlockTag("netherite_blocks");
+
     // "tooltiprareness" tags
     public static final TagKey<Item> TOOLTIP_RARENESS_EPIC_ITEM = ModTags.itemTag(new ResourceLocation("tooltiprareness", "epic_item"));
     public static final TagKey<Item> TOOLTIP_RARENESS_LEGENDARY_ITEM = ModTags.itemTag(new ResourceLocation("tooltiprareness", "legendary_item"));
+
+    private static TagKey<Item> commonItemTag(String key)
+    {
+        return ModTags.itemTag(new ResourceLocation("c", key));
+    }
+
+    private static TagKey<Block> commonBlockTag(String key)
+    {
+        return ModTags.blockTag(new ResourceLocation("c", key));
+    }
 }
