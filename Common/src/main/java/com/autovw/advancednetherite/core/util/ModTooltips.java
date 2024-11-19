eefc49d685c5a345af90e755962f818f72ec66aa
Autovw <68228006+Autovw@users.noreply.github.com>
2024-11-19 12:09:48 +0100

    ⚡ Improve tooltips to better highlight positive perks

--- Common/src/main/java/com/autovw/advancednetherite/core/util/ModTooltips.java
+++ Common/src/main/java/com/autovw/advancednetherite/core/util/ModTooltips.java
@@ -12,28 +12,34 @@ import net.minecraft.resources.ResourceLocation;
  */
 public final class ModTooltips
 {
+    /* Misc */
+    private static final MutableComponent PLUS_TOOLTIP = TooltipBuilder.create(id("misc.plus")).withStyle(ChatFormatting.GREEN, ChatFormatting.BOLD);
+    private static final MutableComponent SHIFT_TOOLTIP = TooltipBuilder.create(id("misc.shift")).withStyle(ChatFormatting.YELLOW);
+    public static final MutableComponent SHIFT_KEY_TOOLTIP = TooltipBuilder.create(id("misc.press_shift_key"), SHIFT_TOOLTIP);
+
     /* Armor related tooltips */
-    public static final MutableComponent ENDERMAN_PASSIVE_TOOLTIP = TooltipBuilder.create(ResourceLocation.fromNamespaceAndPath(AdvancedNetherite.MOD_ID, "armor.enderman_passive")).withStyle(ChatFormatting.DARK_GREEN);
-    public static final MutableComponent PIGLIN_PASSIVE_TOOLTIP = TooltipBuilder.create(ResourceLocation.fromNamespaceAndPath(AdvancedNetherite.MOD_ID, "armor.piglin_passive")).withStyle(ChatFormatting.GOLD);
-    public static final MutableComponent PHANTOM_PASSIVE_TOOLTIP = TooltipBuilder.create(ResourceLocation.fromNamespaceAndPath(AdvancedNetherite.MOD_ID, "armor.phantom_passive")).withStyle(ChatFormatting.GRAY);
+    public static final MutableComponent ENDERMAN_PASSIVE_TOOLTIP = TooltipBuilder.create(id("armor.enderman_passive"), PLUS_TOOLTIP).withStyle(ChatFormatting.DARK_GREEN);
+    public static final MutableComponent PIGLIN_PASSIVE_TOOLTIP = TooltipBuilder.create(id("armor.piglin_passive"), PLUS_TOOLTIP).withStyle(ChatFormatting.GOLD);
+    public static final MutableComponent PHANTOM_PASSIVE_TOOLTIP = TooltipBuilder.create(id("armor.phantom_passive"), PLUS_TOOLTIP).withStyle(ChatFormatting.GRAY);
 
     /* Tool related tooltips */
     // Hoes
-    public static final MutableComponent ADDITIONAL_CROP_DROPS_TOOLTIP = TooltipBuilder.create(ResourceLocation.fromNamespaceAndPath(AdvancedNetherite.MOD_ID, "hoe.additional_crop_drops"));
+    public static final MutableComponent ADDITIONAL_CROP_DROPS_TOOLTIP = TooltipBuilder.create(id("hoe.additional_crop_drops"), PLUS_TOOLTIP);
 
     // Pickaxes
-    public static final MutableComponent DIAMOND_ORE_DROP_TOOLTIP = TooltipBuilder.create(ResourceLocation.fromNamespaceAndPath(AdvancedNetherite.MOD_ID, "pickaxe.additional_diamond_drop")).withStyle(ChatFormatting.AQUA);
-    public static final MutableComponent EMERALD_ORE_DROP_TOOLTIP = TooltipBuilder.create(ResourceLocation.fromNamespaceAndPath(AdvancedNetherite.MOD_ID, "pickaxe.additional_emerald_drop")).withStyle(ChatFormatting.DARK_GREEN);
-    public static final MutableComponent GOLD_ORE_DROP_TOOLTIP = TooltipBuilder.create(ResourceLocation.fromNamespaceAndPath(AdvancedNetherite.MOD_ID, "pickaxe.additional_gold_drop")).withStyle(ChatFormatting.GOLD);
-    public static final MutableComponent IRON_ORE_DROP_TOOLTIP = TooltipBuilder.create(ResourceLocation.fromNamespaceAndPath(AdvancedNetherite.MOD_ID, "pickaxe.additional_iron_drop")).withStyle(ChatFormatting.GRAY);
+    public static final MutableComponent DIAMOND_ORE_DROP_TOOLTIP = TooltipBuilder.create(id("pickaxe.additional_diamond_drop"), PLUS_TOOLTIP).withStyle(ChatFormatting.AQUA);
+    public static final MutableComponent EMERALD_ORE_DROP_TOOLTIP = TooltipBuilder.create(id("pickaxe.additional_emerald_drop"), PLUS_TOOLTIP).withStyle(ChatFormatting.DARK_GREEN);
+    public static final MutableComponent GOLD_ORE_DROP_TOOLTIP = TooltipBuilder.create(id("pickaxe.additional_gold_drop"), PLUS_TOOLTIP).withStyle(ChatFormatting.GOLD);
+    public static final MutableComponent IRON_ORE_DROP_TOOLTIP = TooltipBuilder.create(id("pickaxe.additional_iron_drop"), PLUS_TOOLTIP).withStyle(ChatFormatting.GRAY);
 
     // Swords
-    public static final MutableComponent ENDERMAN_MOB_DROP_TOOLTIP = TooltipBuilder.create(ResourceLocation.fromNamespaceAndPath(AdvancedNetherite.MOD_ID, "sword.additional_enderman_mob_drop")).withStyle(ChatFormatting.DARK_GREEN);
-    public static final MutableComponent PIGLIN_MOB_DROP_TOOLTIP = TooltipBuilder.create(ResourceLocation.fromNamespaceAndPath(AdvancedNetherite.MOD_ID, "sword.additional_piglin_mob_drop")).withStyle(ChatFormatting.GOLD);
-    public static final MutableComponent ZOMBIFIED_PIGLIN_MOB_DROP_TOOLTIP = TooltipBuilder.create(ResourceLocation.fromNamespaceAndPath(AdvancedNetherite.MOD_ID, "sword.additional_zombified_piglin_mob_drop")).withStyle(ChatFormatting.GOLD);
-    public static final MutableComponent PHANTOM_MOB_DROP_TOOLTIP = TooltipBuilder.create(ResourceLocation.fromNamespaceAndPath(AdvancedNetherite.MOD_ID, "sword.additional_phantom_mob_drop")).withStyle(ChatFormatting.GRAY);
+    public static final MutableComponent ENDERMAN_MOB_DROP_TOOLTIP = TooltipBuilder.create(id("sword.additional_enderman_mob_drop"), PLUS_TOOLTIP).withStyle(ChatFormatting.DARK_GREEN);
+    public static final MutableComponent PIGLIN_MOB_DROP_TOOLTIP = TooltipBuilder.create(id("sword.additional_piglin_mob_drop"), PLUS_TOOLTIP).withStyle(ChatFormatting.GOLD);
+    public static final MutableComponent ZOMBIFIED_PIGLIN_MOB_DROP_TOOLTIP = TooltipBuilder.create(id("sword.additional_zombified_piglin_mob_drop"), PLUS_TOOLTIP).withStyle(ChatFormatting.GOLD);
+    public static final MutableComponent PHANTOM_MOB_DROP_TOOLTIP = TooltipBuilder.create(id("sword.additional_phantom_mob_drop"), PLUS_TOOLTIP).withStyle(ChatFormatting.GRAY);
 
-    /* Misc */
-    private static final MutableComponent SHIFT_TOOLTIP = TooltipBuilder.create(ResourceLocation.fromNamespaceAndPath(AdvancedNetherite.MOD_ID, "misc.shift")).withStyle(ChatFormatting.YELLOW);
-    public static final MutableComponent SHIFT_KEY_TOOLTIP = TooltipBuilder.create(ResourceLocation.fromNamespaceAndPath(AdvancedNetherite.MOD_ID, "misc.press_shift_key"), SHIFT_TOOLTIP);
+    private static ResourceLocation id(String key)
+    {
+        return ResourceLocation.fromNamespaceAndPath(AdvancedNetherite.MOD_ID, key);
+    }
 }
