6b7181864c4914e8c691c94db86b1e5e37fd5790
Autovw <68228006+Autovw@users.noreply.github.com>
2024-12-28 19:00:18 +0100

    🔥 Remove Detail Armor Bar support due to texture bug
    #49

--- Fabric/src/main/java/com/autovw/advancednetherite/client/ClientHandler.java
+++ Fabric/src/main/java/com/autovw/advancednetherite/client/ClientHandler.java
@@ -1,7 +1,5 @@
 package com.autovw.advancednetherite.client;
 
-import com.autovw.advancednetherite.AdvancedNetherite;
-import com.autovw.advancednetherite.config.ConfigHelper;
 import net.fabricmc.api.ClientModInitializer;
 
 /**
@@ -12,9 +10,5 @@ public class ClientHandler implements ClientModInitializer
     @Override
     public void onInitializeClient()
     {
-        if (AdvancedNetherite.getPlatformHelper().isModLoaded("detailab") && !ConfigHelper.get().getClient().forceDisableDetailArmorBarSupport())
-        {
-            DetailArmorBarSupport.register();
-        }
     }
 }
